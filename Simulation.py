880a0b68f0edcf38fbbc1a9f99bb56f42e3a93db
nathanclough <nathanc@betenbough.com>
2020-11-28 17:44:32 -0600

    Modified to not recreate threads for each step by using events and overiding the threading class

--- Simulation.py
+++ Simulation.py
@@ -1,39 +1,82 @@
 import threading
+import math
+
 CurrentStep = []
 MaxRow = 0
 MaxCol = 0
 NextStep = []
+OutputPath = ""
+events = []
+Continue = threading.Event()
 
 
-def Run(ThreadCount, InputPath, OutputPath):
+def Run(ThreadCount, InputPath, outputPath):
     CreateMatrix(InputPath)
     global MaxRow
     global MaxCol
     global CurrentStep
     global NextStep
+    global OutputPath
+    global events
 
+    OutputPath = outputPath
     # Initialize the next step to the current
     MaxRow = len(CurrentStep)-1
     MaxCol = len(CurrentStep[0])-2  # sutract 1 for /n and one for indexstart 0
     # InitializeNextStep to be current
     NextStep = [[char for char in row] for row in CurrentStep]
 
-    WriteMatrix(OutputPath)
-    # Create a matrix from the input file
-    # iterate through currentStep and write values to next step
-    for x in range(100):
-        # Initialize empty matrix for tracking changes
-        for row in range(MaxRow+1):
-            for col in range(MaxCol+1):
-                CalculateNextState(row, col)
-        # set CurrentStep = to step Calculated in NextStep
-        CurrentStep = [[char for char in row] for row in NextStep]
-        WriteMatrix(OutputPath)
-    # Iterate through the matrix simulating the next step
-    WriteMatrix(OutputPath)
+    # Partition the rows for thread assignments
+    part = math.floor(len(CurrentStep)/ThreadCount)
+    start = 0
+    end = part
 
-# Given the row, col index in CurrentStep calculates
-# the next step for that position and writes it to NextStep
+    # Create the threads
+    threads = []
+    events = []
+    # For thread count - 1 add partition and add to the pool
+    for x in range(ThreadCount-1):
+        e = threading.Event()
+        t = SimulationThread(start, end, e)
+        events.append(e)
+        threads.append(t)
+        start += part
+        end += part
+
+    # Add the final thread truncing it to the MaxRow rather than end because end is > than size of
+    e = threading.Event()
+    t = SimulationThread(start, MaxRow+1, e)
+    events.append(e)
+    threads.append(t)
+
+    for thread in threads:
+        thread.start()
+
+    for x in range(100):
+        # Wait for  all the threads to finish calculating
+        for event in events:
+            event.wait()
+
+        for row in range(MaxRow+1):
+            for col in range(MaxCol + 1):
+                CurrentStep[row][col] = NextStep[row][col]
+
+        # Reset the events for this round
+        for event in events:
+            event.clear()
+
+        # Notify them they can do the next step
+
+        Continue.set()
+        # Reset for the next run
+        Continue.clear()
+
+    WriteMatrix()
+    SimulationThread.running = False
+    Continue.set()
+
+
+# Given a start row and end row this function will calculate next state for those rows
 
 
 def CalculateNextState(row, col):
@@ -93,8 +136,26 @@ def CreateMatrix(InputPath):
         CurrentStep.append([char for char in row])
 
 
-def WriteMatrix(OutputPath):
+def WriteMatrix():
     outputFile = open(OutputPath, 'w')
     for row in CurrentStep:
         for char in row:
             print(char, sep="", end="", file=outputFile)
+
+
+class SimulationThread(threading.Thread):
+    running = True
+
+    def __init__(self, StartRow, EndRow, CompletedStep):
+        threading.Thread.__init__(self)
+        self.StartRow = StartRow
+        self.EndRow = EndRow
+        self.CompletedStep = CompletedStep
+
+    def run(self):
+        while(self.running):
+            for row in range(self.StartRow, self.EndRow):
+                for col in range(MaxCol + 1):
+                    CalculateNextState(row, col)
+            self.CompletedStep.set()
+            Continue.wait()
